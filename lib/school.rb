c4c6867521d7381c47964d964e5cd52d027f1a28
Jeremy Macias <jeremypmacias@gmail.com>
2020-11-03 11:40:11 -0700

    commit tree c5556f3f20d157b6e076dd716841e81e36781f13

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
 # code here!
+class School
+
+    attr_accessor :name, :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(student_name, grade)
+        roster[grade] ||=[]
+        roster[grade] << student_name
+    end
+
+    def grade(grade)
+        roster.detect do |a, b|
+            if a == grade
+                return b
+            end
+        end
+    end
+
+    def sort
+        sorted_hash = {}
+        roster.each do |a, b|
+            sorted_hash[a] = b.sort
+        end
+        sorted_hash
+    end
+
+
+
+
+end
